42b05dec0583aa31f203e6cd87cb38e15512b08a
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-09-05 19:27:26 -0300

    Cobrindo teste na classe main

---
+++ backend/src/test/java/com/heitor/venda/VendaApplicationTest.java
@@ -0,0 +1,15 @@
+package com.heitor.venda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class VendaApplicationTest {
+
+    @Test
+    void Main(){
+        VendaApplication.main(new String[]{});
+    }
+
+}
