b3bbacfc4f7c19c54e3de2288c7b407df75087f1
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-09-07 11:02:35 -0300

    Implementando teste

--- backend/src/test/java/com/heitor/venda/service/CategoriaServiceTest.java
+++ backend/src/test/java/com/heitor/venda/service/CategoriaServiceTest.java
@@ -2,6 +2,7 @@ package com.heitor.venda.service;
 
 import com.heitor.venda.builder.CategoriaBuilder;
 import com.heitor.venda.domain.Categoria;
+import com.heitor.venda.domain.Produto;
 import com.heitor.venda.repository.CategoriaRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class CategoriaServiceTest {
@@ -23,8 +26,10 @@ class CategoriaServiceTest {
         Mockito.doReturn(CategoriaBuilder.criarOptional()).when(repo).findById(Mockito.anyInt());
 
         Categoria categoria = service.findById(1);
-
+        List<Produto> produtos = categoria.getProdutos();
+        
         Assertions.assertEquals(Categoria.class, categoria.getClass());
+        Assertions.assertNotNull(produtos);
     }
 
 }
