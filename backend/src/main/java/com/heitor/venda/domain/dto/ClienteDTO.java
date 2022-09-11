27095880bec7188ef1444c11308b35fee5ff22a8
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-09-11 18:54:21 -0300

    Exponde servicos clientes

--- backend/src/main/java/com/heitor/venda/domain/dto/ClienteDTO.java
+++ backend/src/main/java/com/heitor/venda/domain/dto/ClienteDTO.java
@@ -4,20 +4,28 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.heitor.venda.enums.TipoCliente;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
 public class ClienteDTO {
 
     private Integer id;
+    @NotEmpty(message = "Campo obrigatorio")
+    @Length(min = 5, max = 80, message = "Campo deve conter de 5 a 80 caracteres")
     private String nome;
+    @NotEmpty(message = "Campo obrigatorio")
     private String email;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String cpfOuCnpj;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private TipoCliente tipo;
+    private Set<String> telefones = new HashSet<>();
     private List<EnderecoDTO> enderecoList = new ArrayList<>();
 }
--- backend/src/main/java/com/heitor/venda/resource/ClienteResources.java
+++ backend/src/main/java/com/heitor/venda/resource/ClienteResources.java
@@ -1,13 +1,15 @@
 package com.heitor.venda.resource;
 
 import com.heitor.venda.domain.Cliente;
+import com.heitor.venda.domain.dto.ClienteDTO;
 import com.heitor.venda.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
@@ -20,8 +22,41 @@ public class ClienteResources {
         this.service = service;
     }
 
+    @PostMapping
+    public ResponseEntity<Cliente> save(@RequestBody @Valid Cliente cliente){
+        return ResponseEntity.ok(service.save(cliente));
+    }
+
+    @PutMapping
+    public ResponseEntity<ClienteDTO> update(@RequestBody @Valid ClienteDTO clienteDTO){
+        return ResponseEntity.ok(service.update(clienteDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
     @GetMapping("/{idCliente}")
     public ResponseEntity<Cliente> findById(@PathVariable(name = "idCliente") Integer idCliente){
         return ResponseEntity.ok(service.findById(idCliente));
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<Cliente>> pageCliente(
+            @RequestParam(name = "page",        defaultValue = "0")     Integer page,
+            @RequestParam(name = "size",        defaultValue = "24")    Integer size,
+            @RequestParam(name = "direction",   defaultValue = "ASC")   String direction,
+            @RequestParam(name = "orderBy",     defaultValue = "NOME")  String orderBy){
+        return ResponseEntity.ok(service.pageCliente(page, size, direction, orderBy));
+    }
+
+    @GetMapping("/pageDto")
+    public ResponseEntity<Page<ClienteDTO>> pageClienteDto(
+            @RequestParam(name = "page",        defaultValue = "0")     Integer page,
+            @RequestParam(name = "size",        defaultValue = "24")    Integer size,
+            @RequestParam(name = "direction",   defaultValue = "ASC")   String direction,
+            @RequestParam(name = "orderBy",     defaultValue = "NOME")  String orderBy){
+        return ResponseEntity.ok(service.pageClienteDto(page, size, direction, orderBy));
     }
 }
