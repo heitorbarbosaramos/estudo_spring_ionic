8b4a13ded3d0e591a88e2a342e4059a2f2b0a9f5
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-09-13 08:07:46 -0300

    Adcionando read only em Produto Repository

--- backend/src/main/java/com/heitor/venda/repository/ProdutoRepository.java
+++ backend/src/main/java/com/heitor/venda/repository/ProdutoRepository.java
@@ -9,11 +9,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+    @Transactional(readOnly = true)
     @Query("SELECT DISTINCT prod FROM Produto prod INNER JOIN prod.categorias cat WHERE prod.nome LIKE %:nome% AND cat IN :cats")
     Page<Produto> search(@Param("nome") String nome, @Param("cats") List<Categoria> cats, Pageable pageRequest);
 }
