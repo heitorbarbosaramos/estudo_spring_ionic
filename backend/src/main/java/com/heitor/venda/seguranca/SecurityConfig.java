cccaa2a5de0b8a71f2c183a98bb44e5d796008c3
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-09-17 21:43:05 -0300

    Implementando autenticacao e geracao do token

--- backend/src/main/java/com/heitor/venda/seguranca/SecurityConfig.java
+++ backend/src/main/java/com/heitor/venda/seguranca/SecurityConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -25,6 +27,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -36,6 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable();//COMO A APLICACAO É STATELLES, NAO ARMAZENA SESSAO, ESTOU DESABILITANDO
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//PARA ASSEGURAR QUE O BACK NÃO ARMAZENE SESSAO
+
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 
         http.authorizeRequests()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
@@ -56,5 +64,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder byBCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(byBCryptPasswordEncoder());
     }
 }
--- backend/src/main/java/com/heitor/venda/seguranca/SpringUserSecurity.java
+++ backend/src/main/java/com/heitor/venda/seguranca/SpringUserSecurity.java
@@ -1,9 +1,13 @@
 package com.heitor.venda.seguranca;
 
+import com.heitor.venda.enums.PerfilCliente;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SpringUserSecurity implements UserDetails {
 
@@ -11,6 +15,17 @@ public class SpringUserSecurity implements UserDetails {
     private String email;
     private String senha;
     private Collection<? extends GrantedAuthority> authorities;
+
+    public SpringUserSecurity(){
+
+    }
+
+    public SpringUserSecurity(Integer id, String email, String senha, Set<PerfilCliente> perfis) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+        this.authorities = perfis.stream().map(x-> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toList());
+    }
 
     public Integer getId(){
         return id;
